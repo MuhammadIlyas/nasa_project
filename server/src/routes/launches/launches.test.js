671e91438a28c08c1db64308f16302c035050adf
Muhammad Ilyas <muhammad.ilyas@RT-LT-314.rolustech.net>
2023-08-15 14:13:51 +0500

    Update Tests

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -19,72 +19,71 @@ describe("Launches API", () => {
     });
   });
 
-//   describe("Test GET /launches", () => {
-//     test("It should return response with 200", async () => {
-//       const response = await request(app)
-//         .get("/launches")
-//         .expect(200)
-//         .expect("Content-Type", /json/);
-//     });
-//   });
+   describe("Test GET /launches", () => {
+     test("It should return response with 200", async () => {
+       const response = await request(app)
+         .get("/launches")
+         .expect(200)
+         .expect("Content-Type", /json/);
+     });
+   });
 
-//   describe("Test POST /launches", () => {
-//     const launch = {
-//       mission: "ABC",
-//       rocket: "B",
-//       launchDate: "January 10, 2023",
-//       target: "kepler-62 f",
-//     };
+   describe("Test POST /launches", () => {
+     const launch = {
+       mission: "ABC",
+       rocket: "B",
+       launchDate: "January 10, 2023",
+       target: "kepler-62 f",
+     };
 
-//     const launchWithoutDate = {
-//       mission: "ABC",
-//       rocket: "B",
-//       target: "kepler-62 f",
-//     };
+     const launchWithoutDate = {
+       mission: "ABC",
+       rocket: "B",
+       target: "kepler-62 f",
+     };
 
-//     const launchWithInvalidDate = {
-//       mission: "ABC",
-//       rocket: "B",
-//       launchDate: "helo",
-//       target: "kepler-62 f",
-//     };
+     const launchWithInvalidDate = {
+       mission: "ABC",
+       rocket: "B",
+       launchDate: "helo",
+       target: "kepler-62 f",
+     };
 
-//     test("It should return response with 201", async () => {
-//       const response = await request(app)
-//         .post("/launches")
-//         .send(launch)
-//         .expect(201)
-//         .expect("Content-Type", /json/);
+     test("It should return response with 201", async () => {
+       const response = await request(app)
+         .post("/launches")
+         .send(launch)
+         .expect(201)
+         .expect("Content-Type", /json/);
 
-//       const requestDate = new Date(launch.launchDate).valueOf();
-//       const responseDate = new Date(response.body.launchDate).valueOf();
+       const requestDate = new Date(launch.launchDate).valueOf();
+       const responseDate = new Date(response.body.launchDate).valueOf();
 
-//       expect(requestDate).toBe(responseDate);
-//       expect(response.body).toMatchObject = launchWithoutDate;
-//     });
+       expect(requestDate).toBe(responseDate);
+       expect(response.body).toMatchObject = launchWithoutDate;
+     });
+     test("Catch missing required properties", async () => {
+       const response = await request(app)
+         .post("/launches")
+         .send(launchWithoutDate)
+         .expect(400)
+         .expect("Content-Type", /json/);
 
-//     test("Catch missing required properties", async () => {
-//       const response = await request(app)
-//         .post("/launches")
-//         .send(launchWithoutDate)
-//         .expect(400)
-//         .expect("Content-Type", /json/);
+       expect(response.body).toStrictEqual({
+         error: "Missing required launch properties",
+       });
+     });
 
-//       expect(response.body).toStrictEqual({
-//         error: "Missing required launch properties",
-//       });
-//     });
+     test("Invalid Date", async () => {
+       const response = await request(app)
+         .post("/launches")
+         .send(launchWithInvalidDate)
+         .expect(400)
+         .expect("Content-Type", /json/);
 
-//     test("Invalid Date", async () => {
-//       const response = await request(app)
-//         .post("/launches")
-//         .send(launchWithInvalidDate)
-//         .expect(400)
-//         .expect("Content-Type", /json/);
-
-//       expect(response.body).toStrictEqual({
-//         error: "Invalid launch date",
-//       });
-//     });
-//   });
+       expect(response.body).toStrictEqual({
+         error: "Invalid launch date",
+       });
+     });
+   });
 });
