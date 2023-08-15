0e63914163f2f479a670f122b785f0a694bb93ed
Muhammad Ilyas <muhammad.ilyas@RT-LT-314.rolustech.net>
2023-08-15 14:09:59 +0500

    Change the Test

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -2,9 +2,11 @@ const request = require("supertest");
 const app = require("../../app");
 const { mongoConnect , mongoDisconnect} = require("../../services/mongo");
 
+const {loadPlanetsData} = require("../../models/planets.model");
 describe("Launches API", () => {
     beforeAll(async () => {
       await mongoConnect();
+      await loadPlanetsData();
     }, 30000);
     afterAll(async () => {
       await mongoDisconnect();
